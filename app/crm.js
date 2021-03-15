b40be857d410713f4933c5e274d7e557616903d8
Ian McAllister <ianmcallister@Ians-Air.home>
2021-03-15 14:29:51 -0700

    Fix: GetCrmMerchIdViaSqMrchId

--- app/crm.js
+++ app/crm.js
@@ -20,8 +20,13 @@ async function GetCrmMerchIdViaSqMrchId(sq_merchant_id) {
     //  NOTIFY PROGRESS
     console.log('getting Merchent id from square merchant id: ', sq_merchant_id);
 
+    //  1. COLLECT MERCHANT ID
+    var crmMerchId = await Firebase.get.crmMerchIdviaSqMrchId(sq_merchant_id);
+        // NOTIFY PROGESS
+        console.log('gto this crmMerchId @ GetCrmMerchIdViaSqMrchId: ', crmMerchId);
+
     //  RETURN
-    return await Firebase.get.crmMerchIdviaSqMrchId(sq_merchant_id);;
+    return crmMerchId 
 };
 
 /*
