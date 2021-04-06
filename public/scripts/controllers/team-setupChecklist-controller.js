1815dc4e2864303b6b10d469ae9558ed2fef8f5d
Ian McAllister <ianmcallister@Ians-Air.home>
2021-04-05 20:50:36 -0700

    Add: Setup Checklist

--- public/scripts/controllers/team-setupChecklist-controller.js
+++ public/scripts/controllers/team-setupChecklist-controller.js
@@ -1,15 +1,34 @@
 ckc
     .controller('teamSetupChecklistController', teamSetupChecklistController);
 
-	teamSetupChecklistController.$inject = ['$scope','$log', '$routeParams'];
+	teamSetupChecklistController.$inject = ['$scope','$log', '$routeParams', '$firebaseObject'];
 
 /* @ngInject */
-function teamSetupChecklistController($scope, $log, $routeParams) {
-
-	//define view model variable
-	var vm = this;
-
+function teamSetupChecklistController($scope, $log, $routeParams, $firebaseObject) {
+	//	NOTIFY PROGRESS
 	$log.info('in the setup checklist controller', $routeParams);	    //  TODO: TAKE THIS OUT LATER
 
+	//	DEFINE: PRIVATE VARIABLES
+	var _path = 'Actualizations/Mfg/' + $routeParams.shippingReportId;
+	var _db = firebase.database();
+	var _reportRef = _db.ref(_path);
 
-}
+	//	DEFINE: VIEW MODEL VARIBLES
+	var vm = this;
+	var report = $firebaseObject(_reportRef);
+
+	//	bind to the scope
+	report.$bindTo($scope, 'vm.report')
+	.then(function() {
+
+	});
+
+	//	DEFINE: PRIVATE FUNCTIONS
+	//	DEFINE: VIEW MODEL FUNCITONS
+
+	
+	
+	
+
+
+};
